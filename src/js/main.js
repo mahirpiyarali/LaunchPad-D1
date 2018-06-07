cf4d806dbc633f0416a771300433a4a1f3a9872b
Mahir Piyarali UPrep <mpiyarali@universityprep.org>
2018-06-06 22:49:43 -0700

    i

--- src/js/main.js
+++ src/js/main.js
@@ -17,7 +17,8 @@ window.cityData.forEach(function(row){
     riseOnHover: true,
 
     icon: L.divIcon({
-      className: "city-marker"
+      className: "city-marker",
+      html: "<div class=dot></div>"
     })
   });
 
@@ -31,9 +32,11 @@ row.marker = marker;
   <li> Overall job growth (last 10 years): <b>${row.j_g_p}</b>
   <li> Job Growth in Tech Sector (last 5 years): <b>${row.t_g_p}</b>
   <li> Offered tax incentives: <b>${row.tax_in}</b>
-  <li> State's Political Lean: <b>${row.lean}</b>
-  <li> Traffic congestion: <b>${row.traftext}</b>
   <li> Airport(s) that meet requirements: <b>${row.Airport_Name}</b>
+  <li> Traffic congestion: <b>${row.traftext}</b>
+  <li> Median rent: <b>${row.rentdisp}</b>
+  <li> Cultural diversity ranking (out of all cities in USA): <b>${row.cds}</b>
+  <li> State's Political Lean: <b>${row.lean}</b>
 </ul>
   `);
   marker.addTo(markerLayer);
@@ -41,30 +44,40 @@ row.marker = marker;
 });
 
 markerLayer.addTo(map);
-//map.fitBounds(markerLayer.getBounds());
-map.fitBounds([[50.792,-129.7],[18.64,-62.66]]);
 
 var blocks = $(".map-item").reverse();
 var filters = {
   j_growth: d=>d.j_growth > .05,
   t_growth: d=>d.tech_growth > .06 && d.j_growth > .05,
   rent: d=>d.rent < 1150,
-  airports: d=>d.Airport > 0,
-  traffic: d=>d.Traffic > 10,
+  airports: d=>d.Airport < 1,
+  traffic: d=>d.Traffic < 11,
   incentives: d=>d.t_in_num > 0,
-  key: d=>d.key > 0,
-  politics: d=>d.swing > 0,
+  key: d=>d.Traffic > 20,
+  swing: d=>d.swing > 0,
+  happy: d=>d.happy > 0,
+  lib: d=>d.lean2 > 0,
+  diverse: d=>d.diverse > 0,
   all: d=>true,
   none:d=>false
 
 }
 
+var cover = $.one(".cover");
 var lockMap = function(lock) {
   var options = ["scrollWheelZoom", "touchZoom"];
   var method = lock ? "disable" : "enable";
   options.forEach(o => map[o][method]());
+  if (lock) {
+    map.removeControl(map.zoomControl);
+    cover.style.display = "block";
+    //map.fitBounds(markerLayer.getBounds());
+    map.fitBounds([[50.792,-129.7],[18.64,-62.66]]);
+  } else {
+    map.addControl(map.zoomControl);
+    cover.style.display = "none";
+  }
 };
-
 var currentFilter = null;
 window.addEventListener("scroll",function(e){
 
@@ -80,13 +93,15 @@ for(var i = 0; i < blocks.length; i++){
     if (filtername == currentFilter) return;
     currentFilter = filtername;
     var filter = filters[filtername];
+    var highlight = el.getAttribute("data-highlight") || "highlight";
     var matches = window.cityData.filter(filter);
-    window.cityData.forEach(d => d.marker.getElement().classList.remove("highlight"));
-    matches.forEach(d=> d.marker.getElement().classList.add("highlight"));
+    window.cityData.forEach(d => d.marker.getElement().classList.remove("highlight", "positive", "negative", "liberal"));
+    matches.forEach(d=> d.marker.getElement().classList.add(highlight));
+    if (!matches.length) return;
     var bounds = L.latLngBounds();
     matches.forEach(m=> bounds.extend(m.marker.getLatLng()));
     //console.log(bounds);
-    map.flyToBounds(bounds, { duration: 1.3 , easeLinearity: .1});
+    //map.flyToBounds(bounds, { duration: 1.3 , easeLinearity: .1});
     //iconSize: [30,30];
   }
 }
@@ -99,5 +114,7 @@ unlockButton.addEventListener("click",function(){
   var unlocked = map.scrollWheelZoom.enabled();
   lockMap(unlocked);
 })
+
+lockMap(true);
 
 // map.on("click", e => console.log(e.latlng));
